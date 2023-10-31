f1e3daca4a0b068c32dd8137371a374d224fcf28
yuris <yls4@cornell.edu>
2023-10-31 13:47:34 -0400

    complete phase 1

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,18 +1,30 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
 import './Greenhouse.css';
-
+import { ThemeContext, useTheme } from '../../context/ThemeContext';
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
 function Greenhouse() {
 
+  const { themeName, setThemeName } = useTheme();
+
+  let src = nightImage;
+
+  if (themeName === 'day') {
+      src = dayImage;
+  }
+
+  const img = <img className='greenhouse-img' src={src} alt='greenhouse'/>;
+
+  // const img = <img  className='greenhouse-img'
+  //           src={dayImage}
+  //           alt='greenhouse' 
+  //           />
+
   return (
     <section>
-      <img  className='greenhouse-img'
-            src={dayImage}
-            alt='greenhouse' 
-      />
+      {img}
       <LightSwitch />
       <ClimateStats />
     </section>
