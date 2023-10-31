43cc1013ccb55b624caeeda3c622d7799c84e64d
yuris <yls4@cornell.edu>
2023-10-31 14:26:52 -0400

    completed phase 3

--- src/context/ClimateContext.js
+++ src/context/ClimateContext.js
@@ -1,2 +1,18 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+
+import { createContext, useContext, useState } from "react";
+
+export const ClimateContext = createContext();
+
+export const useClimateContext = useContext(ClimateContext);
+
+export const ClimateProvider = (props) => {
+    const [climateTemperature, setClimateTemperature] = useState(50);
+
+    return (
+        <ClimateContext.Provider value={{climateTemperature, setClimateTemperature}}>
+            {props.children}
+        </ClimateContext.Provider>
+    )
+}
